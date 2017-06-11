4679458ebe522a2cf2a52fed9e56d8a9ba7b3dbb
IuliiaKot <kotlenko.julia@gmail.com>
2017-06-10 20:02:50 -0400

    add state to App component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,38 @@ import Search from './Search';
 import BusLists from './BusLists';
 import logo from './logo.svg';
 import './App.css';
-import { Button } from 'reactstrap';
+import axios from 'axios';
+import myData from './allstations.json';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      allStations: [],
+      stationInfo: []
+    }
+  }
+
+  componentDidMount(){
+    let allStations = myData.root.stations.station;
+
+    this.setState({
+      allStations: allStations
+    })
+
+  }
+
+  updateStation(data){
+    this.setState({
+      stationInfo: data.root.station[0].etd
+    })
+  }
+
   render() {
     return (
       <div className="center_div">
-          <Search />
-          <BusLists/>
-           {/*<Button color="danger">Danger!</Button>*/}
+          <Search stations={this.state.allStations} handleUpdate={this.updateStation}/>
+          <BusLists stationInfo={this.state.stationInfo}/>
       </div>
     );
   }
--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import { InputGroup, InputGroupButton, Input, Row, Col} from 'reactstrap';
 import axios from 'axios';
-import myData from './allstations.json';
 import Autocomplete from 'react-autocomplete';
 import Select from 'react-select';
 let parseString = require('xml2js').parseString;
@@ -11,30 +10,20 @@ class Search extends Component {
   constructor(){
     super()
     this.state = {
-      allStations: [],
       value: ''
     }
-    this.logChange = this.logChange.bind(this)
+    this.fetchInfoStation = this.fetchInfoStation.bind(this)
   }
 
-  componentDidMount(){
-    let allStations = myData.root.stations.station;
-
-    this.setState({
-      allStations: allStations
-    })
-
-  }
-    logChange(val) {
+    fetchInfoStation(val) {
         this.setState({
             value: (val ? val.value : "")
         })
 
         axios.get('https://api.bart.gov/api/etd.aspx?cmd=etd&orig=19TH&key=MW9S-E7SL-26DU-VV8V')
             .then(res => {
-                parseString(res.data, function (err, result) {
-                    // debugger
-                    console.log(result);
+                parseString(res.data, (err, result)  => {
+                    this.props.handleUpdate(result)
                 });
             })
     }
@@ -47,10 +36,10 @@ class Search extends Component {
            <Select
                 name="form-field-name"
                 value={this.state.value}
-                options={this.state.allStations.map(station => {
-                    return {value: station.abbr, label: station.address}
+                options={this.props.stations.map(station => {
+                    return {value: station.abbr, label: station.name}
                 })}
-                onChange={this.logChange}
+                onChange={this.fetchInfoStation}
                 />
           </div>           
         )
